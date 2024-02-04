ec25d603bb915feceed811e7f55b1b8a59a97a10
Konstantin <kchernyshof@gmail.com>
2024-02-04 16:19:24 +0500

    start

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	// setup watcher
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	// use goroutine to start the watcher
+	go func() {
+		for {
+			select {
+			// provide the list of events to monitor
+			case event := <-watcher.Events:
+				if event.Op&fsnotify.Create == fsnotify.Create {
+					fmt.Println("File created:", event.Name)
+				}
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					fmt.Println("File modified:", event.Name)
+				}
+				if event.Op&fsnotify.Remove == fsnotify.Remove {
+					fmt.Println("File removed:", event.Name)
+				}
+				if event.Op&fsnotify.Rename == fsnotify.Rename {
+					fmt.Println("File renamed:", event.Name)
+				}
+				if event.Op&fsnotify.Chmod == fsnotify.Chmod {
+					fmt.Println("File permissions modified:", event.Name)
+				}
+			case err := <-watcher.Errors:
+				log.Println("Error:", err)
+			}
+		}
+	}()
+
+	// provide the directory to monitor
+	err = watcher.AddWith("c:/tmp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
